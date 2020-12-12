990fc3a458eb5e010d4cebb7121a8510245c47e7
hossein nick <hossinnick62@gmail.com>
2020-12-12 17:54:09 +0330

    init spring boot

---
+++ src/main/java/home/train/LiberyApplication.java
@@ -0,0 +1,13 @@
+package home.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberyApplication.class, args);
+	}
+
+}
